773c9a2679a90787f98bed8b959713b31d0fd2b0
andreza naara <naarafreire06@gmail.com>
2023-05-30 12:19:52 -0300

    deixando os dados dinamicos

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css']
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit {
+  photoCover:string = "https://th.bing.com/th/id/R.3f0e177895ffea21ca745ce8702b4c87?rik=tGcTFvw9EGz%2foQ&pid=ImgRaw&r=0"
+  cardTitle:string = "NOVO HOMEM DE FERRO ANUNCIADO"
+  cardDescription:string = "Marvel Studios anuncia novo filme do Homem de Ferro"
 
+  constructor(){ }
+  ngOnInit(): void{
+  }
 }
